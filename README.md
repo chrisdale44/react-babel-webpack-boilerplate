9d509e394704254ea24ad41f55601da24ff4774c
christopherdale <christopher.j.dale@baesystems.com>
2017-01-22 03:43:16 +0000

    add localhost server and js + react linting

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # React babel webpack boilerplate
+
+npm install
+npm start (node server.js)
+npm test (eslint src/**/*.js*)
+webpack --watch
--- src/views/home.js
+++ src/views/home.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { withRouter } from 'react-router';
 // import {mainStyle} from '../style';
 
-class Home extends React.Component<any, any> {
+class Home extends React.Component {
     constructor() {
         super();
         this.state = {
